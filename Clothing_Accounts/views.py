3a9d756bec775c22d3bb1bda6a827569df08a284
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-02-26 23:41:09 +0600

    User Authentication,login, Category part,Category wise single product work has been done. From add to cart,cart length done, mini cart is going on

--- Clothing_Accounts/views.py
+++ Clothing_Accounts/views.py
@@ -55,7 +55,7 @@ def log_out(request):
 
 def home(request):
     catg= CATEGORIES.objects.all()
-    length= len(Cart.objects.filter (user = request.user))
+    length = len(Cart.objects.filter (user = request.user))
     cart_details = Cart.objects.filter (user = request.user)
     return render(request, 'home.html', locals())
 
