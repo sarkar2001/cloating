691503e34f6ea407af02edf0f2c0d2d94833f9a4
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-03-14 23:48:50 +0600

    User Authentication,login, Category part,Category wise single product work,mini cart,add and remove in cart is done, sub category is going on

--- ClothingApp/admin.py
+++ ClothingApp/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Category)
+# admin.site.register(Category)
 # admin.site.register(SubCategory)
-admin.site.register(PRODUCT)
+# admin.site.register(PRODUCT)
 admin.site.register(Cart)
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ['id','category','name']
+
+@admin.register(PRODUCT)
+class PRODUCTAdmin(admin.ModelAdmin):
+    list_display = ['id','title','category','subcategory', 'condition','price']
