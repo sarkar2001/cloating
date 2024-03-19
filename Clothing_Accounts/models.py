4d192851618cde2e021714c62a0a0432b8062050
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-03-19 19:15:04 +0600

    User Authentication,login, Category part,Category wise single product work,mini cart,add and remove in cart, sub category done. subsub category is going on

--- Clothing_Accounts/models.py
+++ Clothing_Accounts/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 class SliderBanner(models.Model):
     image = models.ImageField(upload_to='slider/')
-    sale_offer = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    sale_offer = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, null=True)
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
