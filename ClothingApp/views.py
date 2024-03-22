c42fcda202bd5c1d69c4088885dc11ddade9e8d9
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-03-22 16:45:56 +0600

    User Authentication,login, Category part,Category wise single product work,mini cart,add and remove in cart, sub category, subsub category done. Responsive is going on

--- ClothingApp/views.py
+++ ClothingApp/views.py
@@ -4,12 +4,34 @@ from django.db.models import Q
 # Create your views here.
 
 def product(request, id):
-    prod = PRODUCT.objects.filter(category=id)
+    user = request.user
+    subcategory = SubCategory.objects.get(id=id)
+    category = Category.objects.all()
+    length = len(Cart.objects.filter())
+    cart_details = Cart.objects.filter()[:2]
+    all_cart =Cart.objects.filter()
+    subtotal = 0
+    for i in all_cart:
+        a = i.product.price * i.quantity
+        subtotal += a
+
+    prod = PRODUCT.objects.filter(subcategory=subcategory)
     return render(request, 'Shop/all_product.html', locals())
 
 
 def single_product(request, id):
-    single_product = PRODUCT.objects.get(id=id)
+    user = request.user
+    if user.is_authenticated:
+        length = len(Cart.objects.filter(user=user))
+        cart_details = Cart.objects.filter(user=user)[:2]
+        all_cart = Cart.objects.filter(user=user)
+        subtotal = 0
+        for i in all_cart:
+            a = i.product.price * i.quantity
+            subtotal += a
+
+        single_product = PRODUCT.objects.get(id=id)
+
     return render(request, 'Shop/single_product.html', locals())
 
 def addtocart(request, id):
@@ -48,8 +70,8 @@ def cart(request):
         all_cart = Cart.objects.filter(user=user)
         subtotal = 0
         for i in all_cart:
-            a = i.product.price * i.quantity
-            subtotal += a
+            n= i.product.price * i.quantity
+            subtotal += n
 
     return render(request, 'Shop/shopping_cart.html', locals())
 
@@ -59,7 +81,7 @@ def plus_cart(request, id):
     user = request.user
     if prod:
         if user.is_authenticated:
-            cart = Cart.objects.get(user=user, product=prod,purchased=False)
+            cart = Cart.objects.get(user=user, product=prod)
             if cart:
                 cart.quantity += 1
                 cart.save()
@@ -72,7 +94,7 @@ def minus_cart(request, id):
     user = request.user
     prod = PRODUCT.objects.get(id=id)
     if user.is_authenticated:
-        cart = Cart.objects.get(user=user, product=prod,purchased=False)
+        cart = Cart.objects.get(user=user, product=prod)
         if cart:
             cart.quantity -= 1
             if cart.quantity < 1:
