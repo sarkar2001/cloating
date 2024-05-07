4eef5ca5e3e1647a2a1ab29a169a9f56409e6500
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-05-07 22:23:30 +0600

    User Authentication,login, Category part,Category wise single product work,mini cart,add and remove in cart, sub category, sub-sub category,Hot deal,trending product, brand ,Contact us done. Carousel for all product,Search function , Payment gateway  Pagination done. Order history is going on

--- ClothingApp/views.py
+++ ClothingApp/views.py
@@ -8,16 +8,17 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def product(request, id):
-    user = request.user
+    # user = request.user
     subcategory = SubCategory.objects.get(id=id)
+    subsubcategory = Subsubcategory.objects.get(id=id)
     category = Category.objects.all()
-    length = len(Cart.objects.filter(user=user))
-    cart_details = Cart.objects.filter(user=user)[:2]
-    all_cart = Cart.objects.filter(user=user)
-    subtotal = 0
-    for i in all_cart:
-        a = i.product.price * i.quantity
-        subtotal += a
+    # length = len(Cart.objects.filter(user=user))
+    # cart_details = Cart.objects.filter(user=user)[:2]
+    # all_cart = Cart.objects.filter(user=user)
+    # subtotal = 0
+    # for i in all_cart:
+    #     a = i.product.price * i.quantity
+    #     subtotal += a
 
     prod = PRODUCT.objects.filter(subcategory=subcategory)
     return render(request, 'Shop/all_product.html', locals())
@@ -26,44 +27,67 @@ def product(request, id):
 def single_product(request, id):
     user = request.user
     products = PRODUCT.objects.all()
+
     if user.is_authenticated:
         length = len(Cart.objects.filter(user=user))
         cart_details = Cart.objects.filter(user=user)[:2]
         all_cart = Cart.objects.filter(user=user)
-        subtotal = 0
-        for i in all_cart:
-            a = i.product.price * i.quantity
-            subtotal += a
+        subtotal = sum(item.product.price * item.quantity for item in all_cart)
 
-        single_product = PRODUCT.objects.get(id=id)
+    single_product = PRODUCT.objects.get(id=id)
 
-    return render(request, 'Shop/single_product.html', locals())
+    if user.is_authenticated:
+        context = {
+            'products': products,
+            'length': length,
+            'cart_details': cart_details,
+            'all_cart': all_cart,
+            'subtotal': subtotal,
+            'single_product': single_product
+        }
+    else:
+        context = {
+            'products': products,
+            'single_product': single_product
+        }
+
+    return render(request, 'Shop/single_product.html', context)
+
 
 def addtocart(request, id):
-    prod= PRODUCT.objects.get(id=id)
-    variation = Variation.objects.all()
+    prod = PRODUCT.objects.get(id=id)
     user = request.user
+
     if prod:
         if user.is_authenticated:
-            try:
-                cart = Cart.objects.get (Q(user=user) & Q(product=prod))
-                if cart:
-                    quantity = request.POST.get('quantity')
-                    # size = request.POST.get('size')
-                    # color = request.POST.get('color')
-                    cart.quantity += int(quantity)
-                    # cart.color = colors
-                    # cart.size = sizes
-                    # print('quantity is',cart.quantity)
-                    cart.save()
-                    return redirect(request.META['HTTP_REFERER'])
-            except:
-                    new_cart = Cart.objects.create (user=user,product=prod)
-                    new_cart.save()
-                    return redirect(request.META['HTTP_REFERER'])
+            if request.method == 'POST':
+                # Get variation details from the form
+                variation_id = request.POST.get('variation')
+                size = request.POST.get('size')
+                color = request.POST.get('color')
+                quantity = int(request.POST.get('quantity', 1))
+
+                # Fetch the variation object
+                variation = Variation.objects.get(id=variation_id)
+
+                # Check if the item already exists in the cart
+                cart_item = Cart.objects.filter(
+                    Q(user=user) & Q(product=prod) & Q(variation=variation) & Q(size=size) & Q(color=color)).first()
+
+                if cart_item:
+                    # If item already exists, update the quantity
+                    cart_item.quantity += quantity
+                    cart_item.save()
+                else:
+                    # If item doesn't exist, create a new cart entry
+                    Cart.objects.create(user=user, product=prod, variation=variation, size=size, color=color,
+                                        quantity=quantity)
+
+            return redirect(request.META.get('HTTP_REFERER', '/'))
         else:
-            return redirect('Log_in')
-        return redirect(request.META['HTTP_REFERER'])
+            return redirect('Accounts/Log_in.html')  # Redirect to login page if user is not authenticated
+    else:
+        return redirect('home.html')  # Redirect to home page if product does not exist
 
 
 
@@ -140,7 +164,7 @@ def checkout(request):
                     delivery_charge = 0
                     total += delivery_charge
 
-                elif  length > 1:
+                elif length > 1:
                     delivery_charge = 0
                     total += delivery_charge
                 else:
@@ -158,6 +182,8 @@ def checkout(request):
         return redirect(request.META['HTTP_REFERER'])
 
     return render(request, 'Shop/checkout.html', locals())
+
+
 
 
 def sslcommerz(request):
@@ -181,10 +207,7 @@ def sslcommerz(request):
                 'currency': "BDT",
                 'tran_id': "tran_12345",
                 'success_url': "http://127.0.0.1:8020/ClothingApp/success/",
-                # if transaction is succesful, user will be redirected here
-                'fail_url': "http://127.0.0.1:8020/ClothingApp/fail/",  # if transaction is failed, user will be redirected here
-                # 'cancel_url': "http://127.0.0.1:8000/payment-cancelled",
-                # after user cancels the transaction, will be redirected here
+                'fail_url': "http://127.0.0.1:8020/ClothingApp/fail/",
                 'emi_option': "0",
                 'cus_name': "test",
                 'cus_email': "test@test.com",
@@ -218,30 +241,46 @@ def fail(request):
     return render(request, 'Shop/fail.html')
 
 
-def Order(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        address = request.POST.get('address')
-        town_or_city = request.POST.get('town_or_city')
-        state_or_county = request.POST.get('state_or_county')
-        postcode_or_zip = request.POST.get('postcode_or_zip')
-        email_address = request.POST.get('email_address')
-        phone = request.POST.get('phone')
+# def Order(request):
+#     if request.method == 'POST':
+#         first_name = request.POST.get('first_name')
+#         last_name = request.POST.get('last_name')
+#         address = request.POST.get('address')
+#         town_or_city = request.POST.get('town_or_city')
+#         state_or_county = request.POST.get('state_or_county')
+#         postcode_or_zip = request.POST.get('postcode_or_zip')
+#         email_address = request.POST.get('email_address')
+#         phone = request.POST.get('phone')
+#
+#         order = Order(
+#             first_name=first_name,
+#             last_name=last_name,
+#             address=address,
+#             town_or_city=town_or_city,
+#             state_or_county=state_or_county,
+#             postcode_or_zip=postcode_or_zip,
+#             email_address=email_address,
+#             phone=phone
+#         )
+#         order.save()
+#
+#         # Redirect to a success page or do something else
+#         return redirect('order_success')
+#
+#     return render(request, 'Shop/checkout.html')
 
-        order = Order(
-            first_name=first_name,
-            last_name=last_name,
-            address=address,
-            town_or_city=town_or_city,
-            state_or_county=state_or_county,
-            postcode_or_zip=postcode_or_zip,
-            email_address=email_address,
-            phone=phone
-        )
-        order.save()
+#
+# # Example of reducing stock when an order is placed
+# def place_order(request):
+#     # Logic to place order
+#     # Assuming you have a product_id, color, and size in your request
+#     product = Product.objects.get(pk=request.POST['product_id'])
+#     variation = product.stockvariation_set.get(color=request.POST['color'], size=request.POST['size'])
+#     variation.stock_quantity -= 1
+#     variation.save()
 
-        # Redirect to a success page or do something else
-        return redirect('order_success')
 
-    return render(request, 'Shop/checkout.html')
+
+def subsubcategory(request, id):
+    single_product =  Subsubcategory.objects.filter(id=id)
+    return render(request, 'Shop/all_product.html', locals())
