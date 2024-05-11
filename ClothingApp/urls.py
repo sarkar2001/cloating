46ddbe634cf3c1b65d0fdedfc300e21bfd8f034c
Most. Razia Sultana <raziasultana772233@gmail.com>
2024-05-11 11:41:39 +0600

    User Authentication,login, Category part,Category wise single product work,mini cart,add and remove in cart, sub category, sub-sub category,Hot deal,trending product, brand ,Contact us done. Carousel for all product,Search function , Payment gateway  Pagination done. Order history is going on

--- ClothingApp/urls.py
+++ ClothingApp/urls.py
@@ -4,7 +4,8 @@ from.views import *
 urlpatterns = [
     path('product/<int:id>/', product, name='product'),
     path('single_product/<int:id>/', single_product, name='single_product'),
-    path('addtocart/<int:id>/', addtocart, name='addtocart'),
+    # path('addtocart/<int:id>/', addtocart, name='addtocart'),
+    path('addtocart/<int:product_id>/', addtocart, name='addtocart'),
     path('remove_cart/<int:id>/', remove_cart, name='remove_cart'),
     path('cart/', cart, name='cart'),
     path('minus_cart/<int:id>/', minus_cart, name='minus_cart'),
